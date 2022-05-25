7279679e957a5c67475052529baefccfe78e3944
Pedro Bibiano <pedroedubibiano@gmail.com>
2022-05-25 12:55:43 -0300

    react-hook-form

--- src/assets/pages/Admin/Auth/index.tsx
+++ src/assets/pages/Admin/Auth/index.tsx
@@ -1,20 +1,22 @@
 import { ReactComponent as AuthImage } from 'assets/images/Animal.svg';
 import { Route, Routes } from 'react-router-dom';
+import Login from './Login';
 import './styles.css';
 
 const Auth = () => {
   return (
     <div className="auth-container">
       <div className="auth-banner-container">
-        <h1>Cuide do seu Pet</h1>
+        <h1>Criar uma conta é rápido, fácil e gratuito!</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae sunt
+          Com a sua conta da Pegasus você tem acesso a Ofertas exclusivas, e
+          pode criar e gerenciar a sua Assinatura.
         </p>
         <AuthImage />
       </div>
       <div className="auth-form-container">
         <Routes>
-          <Route path="/login" element={<h1>Card de login</h1>} />
+          <Route path="/login" element={<Login />} />
         </Routes>
 
         <Routes>
